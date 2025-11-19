663eaae4f18e83c1227f9410621359af45477cd1
John <zerihal@hotmail.com>
2025-11-19 22:33:08 +0000

    Added file extension methods

---
+++ DocParser/ExtensionMethods/FileExtensions.cs
@@ -0,0 +1,28 @@
+﻿using DocParser.DocSearch;
+using DocParser.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace DocParser.ExtensionMethods
+{
+    /// <summary>
+    /// File extension methods.
+    /// </summary>
+    public static class FileExtensions
+    {
+        /// <summary>
+        /// Converts a collection of IFormFile to a collection of <see cref="IFormFileStream"/> for processing by 
+        /// instance of <see cref="IDocSearcher"/>.
+        /// </summary>
+        /// <param name="files">Collection of <see cref="IFormFile"/>.</param>
+        /// <returns>Collection of <see cref="IFormFileStream"/>.</returns>
+        public static IEnumerable<IFormFileStream> ToFormFiles(this IEnumerable<IFormFile> files)
+        {
+            var filesList = new List<IFormFileStream>();
+
+            foreach (var file in files)
+                filesList.Add(new FormFileStream(file));
+
+            return filesList;
+        }
+    }
+}
