ec3da560a5a747c70a218e359c02c24e0cedb2cc
John Sweet <John.Sweet@mail.nidec.com>
2025-11-24 12:23:19 +0000

    Improvement to factory method

--- DocParser/Factories/DocParserFactory.cs
+++ DocParser/Factories/DocParserFactory.cs
@@ -13,27 +13,42 @@ namespace DocParser.Factories
         /// Creates an appropriate instance of document parser for the file type.
         /// </summary>
         /// <param name="filename">Filename with extension.</param>
+        /// <param name="loadFile">
+        /// Loads the specified file to the created instance of <see cref="IDocParser"/> (default is <see langword="true"/>).
+        /// </param>
         /// <returns>Instance of <see cref="IDocParser"/> appropriate for the file type.</returns>
-        public static IDocParser? CreateDocParserForFile(string filename)
+        public static IDocParser? CreateDocParserForFile(string filename, bool loadFile = true)
         {
             var fileExt = Path.GetExtension(filename).ToLower();
+            IDocParser? docParser = null;
 
             switch (fileExt)
             {
                 case ".docx":
-                    return new WordDocParser();
+                    docParser = new WordDocParser();
+                    break;
 
                 case ".txt":
                 case ".rtf":
-                    return new TextDocParser();
+                    docParser = new TextDocParser();
+                    break;
 
                 case ".doc":
                 case ".odt":
-                    return new GenericDocParser();
+                    docParser = new GenericDocParser();
+                    break;
 
                 default:
                     Logger.LogWarning("Unsupported file format");
                     break;
+            }
+
+            if (docParser != null)
+            {
+                if (loadFile)
+                    docParser.LoadFile(filename);
+
+                return docParser;
             }
 
             return null;
